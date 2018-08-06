09290f52dd3ff60a2c8a3cea18e7ed3a509c848d
Steve Todorov <steve.todorov@carlspring.com>
2018-08-06 16:26:03 +0300

    SB-1196 Handle form errors.

--- src/app/shared/form/form-field-errors/form-field-errors.component.ts
+++ src/app/shared/form/form-field-errors/form-field-errors.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
 import {FormGroup} from '@angular/forms';
 
 import {ApiFormError} from '../../../modules/core/core.model';
@@ -7,7 +7,8 @@ import {ApiFormError} from '../../../modules/core/core.model';
 @Component({
     selector: 'form-field-errors',
     templateUrl: './form-field-errors.component.html',
-    styleUrls: ['./form-field-errors.component.scss']
+    styleUrls: ['./form-field-errors.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FormFieldErrorsComponent implements OnInit {
 
@@ -20,11 +21,40 @@ export class FormFieldErrorsComponent implements OnInit {
     constructor() {
     }
 
-    getErrors() {
+    getApiErrors() {
         if (this.form !== null && this.form.get(this.name)) {
             const field = this.form.get(this.name);
             if (field.errors && field.errors instanceof ApiFormError) {
                 return field.errors.messages;
+            }
+        }
+
+        return [];
+    }
+
+    getFormErrors() {
+        if (this.form !== null && this.form.get(this.name)) {
+            const field = this.form.get(this.name);
+            if (field.errors && !(field.errors instanceof ApiFormError)) {
+                let errors = [];
+
+                Object.keys(field.errors).forEach((key) => {
+                    const error = field.errors[key];
+                    if (key === 'required') {
+                        errors.push('This field is required!');
+                        return;
+                    } else if (key === 'minlength') {
+                        errors.push('This field requires more than ' + error.requiredLength + ' characters');
+                        return;
+                    } else if (key === 'maxlength') {
+                        errors.push('This field requires less than ' + error.requiredLength + ' characters');
+                        return;
+                    } else {
+                        console.error(field.errors[key]);
+                    }
+                });
+
+                return errors;
             }
         }
 
